32df5d592077cf8707584a141476250e3c8e8de8
Abhijit Samanta <abhijit.samanta92@gmail.com>
2017-09-10 00:38:12 +0530

    first commit

---
+++ src/Assignment1.java
@@ -0,0 +1,14 @@
+
+public class Assignment1 {
+      public static void main(String args[]){
+    	  int arr[]=new int[10];
+    	  for(int i=0;i<10;i++){
+    		  arr[i]=i+1;
+    	  }
+    	  for(int i=0;i<arr.length;i++){
+    		  if(arr[i]%2==0){
+    			  System.out.println(arr[i]);
+    		  }
+    	  }
+      }
+}
